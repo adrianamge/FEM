206250cba137ab558d4d44049b1802cd1500fa9c
adrianamge <51022926+adrianamge@users.noreply.github.com>
2022-03-25 16:54:10 +0300

    dbconection

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -1,5 +1,6 @@
 <?php
-$con=mysqli_connect("localhost", "root", "", "femdb");
+// $con=mysqli_connect("localhost", "root", "", "femdb");
+$con = mysqli_init(); mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL); mysqli_real_connect($conn, "familyexpensetracker-server.mysql.database.azure.com", "nrksmpwexd", "2011426618.ab", "test", 3306, MYSQLI_CLIENT_SSL);
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
